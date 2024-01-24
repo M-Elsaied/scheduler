4210d684deb4dd087669a1950279ded38e245232
M-Elsaied <31868939+M-Elsaied@users.noreply.github.com>
2024-01-24 17:41:08 +0300

    Create Service.js

---
+++ models/Service.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const serviceSchema = new Schema({
+    name: { type: String, required: true },
+    duration: { type: Number, required: true }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Service', serviceSchema);
